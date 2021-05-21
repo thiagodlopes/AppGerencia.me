61236ea30f66c7241589f313a61bd9b1615ff92e
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-05-21 12:34:11 -0300

    Adiciona Material Calendar View

--- app/src/main/java/com/thdlopes/appgerenciame/CalendarFragment.java
+++ app/src/main/java/com/thdlopes/appgerenciame/CalendarFragment.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.applandeo.materialcalendarview.EventDay;
+
+import java.util.Calendar;
+import java.util.List;
+
 public class CalendarFragment extends Fragment {
 
+    private CalendarView calendarView;
+    private String pattern = "dd/MM/yyyy";
+    private List<EventDay> events;
+    private Calendar calendar;
+    
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
