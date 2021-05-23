a394c3486e0546e5183c7d9669391d3d06c859b5
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-05-23 16:19:39 -0300

    Adiciona Adapter de Categorias

--- app/src/main/java/com/thdlopes/appgerenciame/CategoryAdapter.java
+++ app/src/main/java/com/thdlopes/appgerenciame/CategoryAdapter.java
@@ -1,9 +1,57 @@
 package com.thdlopes.appgerenciame;
 
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter>() {
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+
+    List<CategoryModel> categoryList;
+    Context context;
+
+    public CategoryAdapter(List<CategoryModel> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_category, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        CategoryModel categories = categoryList.get(position);
+        holder.textViewName.setText(categories.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textViewName);
+        }
+
+
+    }
 
 }
